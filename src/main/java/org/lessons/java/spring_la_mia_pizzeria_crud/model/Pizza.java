3d95da102c89c82307406207aeec8f30bf05b53d
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-03-17 16:32:57 +0100

    feat: created Pizza model and db schema

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,81 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank (message = "nome must not be null, empty or blank")
+    private String nome;
+
+    @Lob
+    private String descrizione;
+
+    private String foto;
+
+    @NotNull
+    @Min(value = 0, message = "prezzo cannot be negative")
+    private BigDecimal prezzo;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getFoto() {
+        return this.foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public BigDecimal getPrezzo() {
+        return this.prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s, prezzo: €%.2f", this.nome, this.prezzo);
+    }
+
+}
