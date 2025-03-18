a52711a540cf4c3af340e6cf3deec7591514bb97
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-03-18 15:53:44 +0100

    feat: pizza create

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzasController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzasController.java
@@ -2,24 +2,38 @@ package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
 
 import java.util.List;
-
+import org.lessons.java.spring_la_mia_pizzeria_crud.SpringLaMiaPizzeriaCrudApplication;
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repo.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 
 @Controller
 @RequestMapping("/pizzas")
 public class PizzasController {
 
+    private final SpringLaMiaPizzeriaCrudApplication springLaMiaPizzeriaCrudApplication;
+
+    private final IndexController indexController;
+
     @Autowired
     private PizzaRepository repo;
+
+    PizzasController(IndexController indexController, SpringLaMiaPizzeriaCrudApplication springLaMiaPizzeriaCrudApplication) {
+        this.indexController = indexController;
+        this.springLaMiaPizzeriaCrudApplication = springLaMiaPizzeriaCrudApplication;
+    }
     
     @GetMapping
     public String index(Model model){
@@ -42,6 +56,26 @@ public class PizzasController {
 
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        Pizza pizza = new Pizza();
+        pizza.setFoto("https://fakeimg.pl/600x400/ff6666/ffffff?text=Pizza");
+        model.addAttribute("pizza", pizza);
+        return "pizzas/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model ){
+
+        if(bindingResult.hasErrors()){
+            return "pizzas/create";
+        }
+
+        repo.save(formPizza);
+
+        return "redirect:/pizzas";
     }
     
 }
